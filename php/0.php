62fa47e0fcc16153ce626cb3c27fa963f2d2be6c
LizCáceres <lizcaceres654@gmail.com>
2024-10-16 20:13:13 -0300

    php1

---
+++ php/0.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<body>
+<?php 
+    // Comentario de linea simple
+    /*Comentario Extendido*/
+    //Variable
+        $variable = 5;
+        echo $variable;
+        echo "<br>"; //ingresar codigo html en php
+        $cadena = "HELLO";
+        print $cadena;
+        echo "<hr>"; //ingresar codigo html en php
+        $cadena2 = "Hola Mundo desde PHP";
+        echo "<h1>$cadena2</h1>";
+?>
+</body>
+</html>
